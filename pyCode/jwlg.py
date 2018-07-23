2b11627ab6d06b1c3179f8c1bee289e56bdce072
lijiyu0219 <lijiyu0219@126.com>
2018-07-23 15:39:13 +0800

    using re to git contents.

--- pyCode/jwlg.py
+++ pyCode/jwlg.py
@@ -3,6 +3,7 @@
 
 import requests
 import getpass
+import re
 from bs4 import BeautifulSoup
 
 
@@ -46,7 +47,7 @@ class Lesson:
             self.time_place.append(lessonTime_Place(tpItems[0],tpItems[1],tpItems[2]))
 
 class Course:
-    def __init__(self, name, index, addr):
+    def __init__(self, index, name, addr):
         self.name = name
         self.int_index = int(index)
         self.index=index
@@ -61,37 +62,26 @@ class Course:
             time_place_info = lesson_info_array[7].find('table')
             self.lessons.append(Lesson(int(lesson_info_array[0].text),lesson_info_array[6].text,time_place_info))
 
-def getContents():
-    pageContent = session.get('http://jwdep.dhu.edu.cn/dhu/commonquery/selectcoursetermcourses.jsp?pageSize=10000&curPage=1')
-    #get bs Object:
-    bsContent = BeautifulSoup(pageContent.text, "html.parser")
-    #find table of coursed
-    tables_in_page = bsContent.find_all("table")
-    #get main table
-    courses = tables_in_page[1].find_all('tr')
-    courses = courses[1].find_all('td')
-    return courses
-
 #TODO:Change Statements to storge in the database directely.
 def main():
-    #raw_contents_info : unorgnized infomation of courses content.
-    #each course item takes up six rows.
-    raw_contents_info = getContents()
+    
+    pageContent = session.get('http://jwdep.dhu.edu.cn/dhu/commonquery/selectcoursetermcourses.jsp?pageSize=10000&curPage=1')
+    pat = re.compile('courseId=(\d{6})&courseName=(.*?)"')
+    contentsPairs = re.findall(pat,pageContent.text)
     #storge each individual course info.
     courses = []
     #Write infomation into a file.
 
     course_count = 0
     print('Collecting infomations...')
-    for i in range(len(raw_contents_info)):
-        if (i % 6 == 1):
-            courses.append(Course(raw_contents_info[i - 1].text, raw_contents_info[i].text, 'http://jwdep.dhu.edu.cn/dhu/commonquery/coursetimetableinfo.jsp?courseId=' + raw_contents_info[i].text))
-            course_count+=1
-            if (course_count%100 == 0):
-                print(str(course_count)+' courses get.')
+    for item in contentsPairs:
+        courses.append(Course(item[0], item[1], 'http://jwdep.dhu.edu.cn/dhu/commonquery/coursetimetableinfo.jsp?courseId=' + item[0]))
+        course_count+=1
+        if (course_count%100 == 0):
+            print(str(course_count)+' courses get.')
     #Storge Course Data
     print(str(course_count)+'coursed in total.\n\nWriting to file...')
-    out_file = open('new_Course_infomation.txt','w')
+    out_file = open('Course_infomation.txt','w')
     for item in courses:
 #       print(item.index+' '+item.name)
         out_file.write(item.index+','+item.name+':\n')
