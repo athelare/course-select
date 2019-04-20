72904d4be01498ac5b05a2256ea29873dae74cb6
athelare <lijiyu0219@126.com>
2019-04-20 23:29:05 +0800

    init

---
+++ src/test/java/com/ljy/courseselect/CourseSelectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ljy.courseselect;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CourseSelectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
