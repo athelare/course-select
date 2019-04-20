6ed5216a2a775642d7596585171d9f8c6478bbc1
athelare <lijiyu0219@126.com>
2019-04-20 23:23:15 +0800

    test

---
+++ src/main/java/com/ljy/courseselect/CourseSelectApplication.java
@@ -0,0 +1,12 @@
+package com.ljy.courseselect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSelectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseSelectApplication.class, args);
+    }
+
+}
